afbc1dde3b167af80d3b137d30cfea1b48c204d6
unknown <mumbimathu@gmail.com>
2021-04-13 08:21:06 -0700

    My First Git Bash

---
+++ Workfile.py
@@ -0,0 +1,11 @@
+def main():
+    file_name = input("Enter the file name: ")
+    infile = open("tabitha.txt","r")
+    for i in range(4):
+        line = infile.readline()
+        print(data)
+
+        #print (line
+
+
+main()
